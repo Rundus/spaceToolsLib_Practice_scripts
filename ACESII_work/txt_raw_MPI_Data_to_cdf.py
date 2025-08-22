d83428067a135516d159ef6a2796e86510116aa4
Rundus <connorfeltman@yahoo.com>
2025-08-22 10:43:17 -0500

    git commit 7

--- ACESII_work/txt_raw_MPI_Data_to_cdf.py
+++ ACESII_work/txt_raw_MPI_Data_to_cdf.py
@@ -11,10 +11,13 @@ from copy import copy
 from glob import glob
 import numpy as np
 import io
+import time
+import datetime as dt
+from copy import deepcopy
 
 
 # Time your code
-# start_time = time.time()
+start_time = time.time()
 
 
 
@@ -22,6 +25,7 @@ import io
 def txt_raw_MPI_Data_to_cdf():
 
     # Load in the .txt files
+    stl.prgMsg('Loading the data')
     path_to_data = 'C:\Data\ACESII\L3\MPI\low\\'
     data_file_names = glob(path_to_data + '*MPI*' +'*.txt')
 
@@ -37,7 +41,12 @@ def txt_raw_MPI_Data_to_cdf():
             for line in f:
                 temp_data.append([float(val) for val in line.split()])
 
-            temp_data = np.array(temp_data).T
+            temp_data = np.array(temp_data,dtype='float64').T
+
+
+        # Correct T0 point of Epoch in temp_data
+        T0 = dt.datetime(1900, 1, 1) # Jan 1st, 1900
+        Epoch = np.array([T0 + dt.timedelta(seconds=tme) for tme in temp_data[0]])
 
         # APPEND file data into data_dict_output
         if 'MPI1' in txtfile:
@@ -50,15 +59,19 @@ def txt_raw_MPI_Data_to_cdf():
             counter = 4
 
         data_dict_output = {**data_dict_output,
-                            **{f'Epoch_MPI{counter}':[np.array(temp_data[0]), {'LABLAXIS':'Epoch','UNITS':None,'DEPEND_0':f'Epoch_MPI{counter}'}],
+                            **{f'Epoch_MPI{counter}':[np.array(Epoch), {'LABLAXIS':'Epoch','UNITS':None,'DEPEND_0':f'Epoch_MPI{counter}'}],
                                f'Vx_rkt_MPI{counter}':[np.array(temp_data[1]), {'LABLAXIS':'Vx in rocket frame','UNITS':'m/s','DEPEND_0':f'Epoch_MPI{counter}'}],
                                f'Vy_rkt_MPI{counter}':[np.array(temp_data[2]), {'LABLAXIS':'Vy in rocket frame','UNITS':'m/s','DEPEND_0':f'Epoch_MPI{counter}'}]}
                             }
+    stl.Done(start_time)
+
 
     # write out the data
+    stl.prgMsg('Writing out the data')
     outputPath = 'C:\Data\ACESII\L3\MPI\low\\'
     file_out_name = 'ACESII_36364_l3_MPI.cdf'
     stl.outputCDFdata(outputPath=outputPath+file_out_name, data_dict=data_dict_output)
+    stl.Done(start_time)
 
 
 ### EXECUTE ###
